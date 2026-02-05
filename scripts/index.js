71f4ed4fd72231ed6b058d30e489d98ad732302a
Misa Liu <misaliu@misaliu.top>
2026-02-05 15:59:16 +0800

    refactor: Remove Wayback Machine stuff

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,4 @@
-var _____WB$wombat$assign$function_____=function(name){return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name))||self[name];};if(!self.__WB_pmw){self.__WB_pmw=function(obj){this.__WB_source=obj;return this;}}{
-let window = _____WB$wombat$assign$function_____("window");
-let self = _____WB$wombat$assign$function_____("self");
-let document = _____WB$wombat$assign$function_____("document");
-let location = _____WB$wombat$assign$function_____("location");
-let top = _____WB$wombat$assign$function_____("top");
-let parent = _____WB$wombat$assign$function_____("parent");
-let frames = _____WB$wombat$assign$function_____("frames");
-let opens = _____WB$wombat$assign$function_____("opens");
-
+;'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
   document.body.classList.add('loaded');
@@ -20,25 +11,3 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('.warning').classList.add('show');
   }, 200);
 });
-}
-
-/*
-     FILE ARCHIVED ON 08:05:18 Dec 27, 2024 AND RETRIEVED FROM THE
-     INTERNET ARCHIVE ON 10:33:20 Feb 04, 2026.
-     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
-
-     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
-     SECTION 108(a)(3)).
-*/
-/*
-playback timings (ms):
-  captures_list: 0.57
-  exclusion.robots: 0.02
-  exclusion.robots.policy: 0.009
-  esindex: 0.011
-  cdx.remote: 10.244
-  LoadShardBlock: 290.56 (3)
-  PetaboxLoader3.datanode: 150.341 (4)
-  PetaboxLoader3.resolve: 307.713 (3)
-  load_resource: 172.421
-*/
